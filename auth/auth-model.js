266e004bd0563158fca2a684bf7ddd301427377a
Lamberto <asghedom.lamberto@gmail.com>
2019-10-18 10:49:30 -0700

    password hashed

---
+++ auth/auth-model.js
@@ -0,0 +1,14 @@
+const db = require('../database/dbConfig.js')
+
+module.exports = {
+    find,
+    addUser
+}
+
+function find() {
+    return db('users')
+}
+
+function addUser(user) {
+    return db('users').insert(user)
+}
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Auth = require('./auth-model.js')
+
+router.get('/', (req, res) => {
+  Auth.find()
+    .then(users => res.status(200).json(users))
+    .catch(error => res.status(500).json({ message: "Error retrieving users." }))
+})
 
 router.post('/register', (req, res) => {
   // implement registration
+  const user = req.body
+  user.password = bcrypt.hashSync(user.password, 12)
+  Auth.addUser(user)
+    .then(id => res.status(200).json(id))
+    .catch(error => res.status(500).json({ message: "Error adding to database." }))
 });
 
 router.post('/login', (req, res) => {
